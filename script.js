ccb461f0650d1413145798d6f330a67aac0b4f3c
443YOSHI\yosin <yosin330@yahoo.co.jp>
2022-11-10 17:54:10 +0900

    スライダー実装、jQファイル追加

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).on("ready",function(){
+    $(".pcslider").slick({
+       autoplay:true,
+       autoplaySpeed:3000,
+    });
+});
+
+$(document).on("ready",function(){
+    $(".spslider").slick({
+       autoplay:true,
+       autoplaySpeed:3000,
+    });
+});
